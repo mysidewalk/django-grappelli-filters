6c56c36c117fbe290f7770095e89cd4c7aea511c
Stephen Christensen <stephen.t.christensen@gmail.com>
2022-10-31 10:09:54 -0500

    PFP-2429 update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         'License :: Freeware',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
