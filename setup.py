0836d358bb7b55e7d68ebe9f7c047b3913ec3e30
Stephen Christensen <stephen.t.christensen@gmail.com>
2022-11-01 15:10:46 -0500

    PFP-2429 bump django-grappelli-filters to 1.0.0 for django 3 upgrade

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-grappelli-filters',
-    version='1.0.0-dev.1',
+    version='1.0.0',
     packages=['grappelli_filters'],
     include_package_data=True,
     install_requires=['django', 'django-grappelli', ],
